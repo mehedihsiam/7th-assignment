3d3e94746110eb602ba2d1d0a90719d9a2588be9
Mehedi Hasan Siam <mehedisiam0022@gmail.com>
2021-09-13 03:00:02 +0600

    update UI

--- js/app.js
+++ js/app.js
@@ -12,18 +12,19 @@ const showProducts = (products) => {
   for (const product of allProducts) {
     const image = product.image;  //product image has been added
     const div = document.createElement("div");
+    div.classList.add('my-4')
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-        <img class="product-image" src=${image}></img>
+        <img class="product-image" src=${image}>
       </div>
-      <h3 class="fs-5">${product.title}</h3>
+      <h3 class="fs-5 fw-bold mt-4">${product.title}</h3>
       <p class="fs-6">Category: ${product.category}</p>
       <h2 class="fw-bold fs-6">Price: $ ${product.price}</h2>
       <small class="d-block fw-bold">Rating: ${product.rating.rate} <i class="fas fa-star text-warning"></i></small>
       <small class="d-block">Total Review: ${product.rating.count}</small>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-warning">add to cart</button>
+      <button id="details-btn" onclick="detailsContainer('${product.id}')" class="btn btn-info">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
@@ -81,3 +82,20 @@ const updateTotal = () => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal;
 };
+
+const detailsContainer = (productID) => {
+  const url = `https://fakestoreapi.com/products/${productID}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayDetails(data))
+}
+
+const displayDetails = (singleProduct) => {
+  const container = document.getElementById('details-container');
+  container.innerHTML = `
+  <img class="product-image" src=${singleProduct.image}>
+  <h5 class="fw-bold text-success">${singleProduct.title}</h5>
+  <p>${singleProduct.description.slice(0, 200)}</p>
+
+  `;
+}
